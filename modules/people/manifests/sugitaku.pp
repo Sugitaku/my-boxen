c98931719d15bad67346c7fc0fdc6f64b0ed0e4e
Sugitaku <info@sugitaku.net>
2013-12-12 21:56:21 +0900

    add os x setting, modules and dotfiles.

--- modules/people/manifests/sugitaku.pp
+++ modules/people/manifests/sugitaku.pp
@@ -1,9 +1,23 @@
 class people::sugitaku {
+
+  # os x
+  include osx::finder::show_all_on_desktop
+
+  include osx::dock::autohide
+  class { 'osx::dock::icon_size':
+    size => 18
+  }
+
+  include osx::no_network_dsstores
+  include osx::software_update
+
+  # local application
   include chrome
   include dropbox
   include iterm2::stable
   include skype
   include transmit
+  include tunnelblick::beta
 
   package {
     'GoogleJapaneseInput':
@@ -17,6 +31,7 @@ class people::sugitaku {
       provider => compressed_app;
   }
 
+  # homebrew
   package {
     [
       'emacs',
@@ -24,5 +39,31 @@ class people::sugitaku {
       'tig',
       'zsh',
     ]:
+  }
+
+  $home = "/Users/${::boxen_user}"
+
+  # dotfile
+  $dotfiles = "${home}/dotfiles"
+  repository { $dotfiles:
+    source => 'Sugitaku/dotfiles',
+  } 
+  exec {  "ln -s dotfiles/.zshrc .zshrc":
+    cwd => $home,
+    creates => "${home}/.zshrc"
+  }
+  exec {  "ln -s dotfiles/.tmux.conf .tmux.conf":
+    cwd => $home,
+    creates => "${home}/.tmux.conf"
+  }
+  file { "${dotfiles}/.emacs.d/conf":
+    ensure => directory
+  }
+  exec {  "ln -s dotfiles/.emacs.d .emacs.d":
+    cwd => $home,
+    creates => "${home}/.emacs.d"
+  }
+  osx_chsh { $::luser:
+    shell => "${boxen::config::homebrewdir}/bin/zsh";
   }
 }
